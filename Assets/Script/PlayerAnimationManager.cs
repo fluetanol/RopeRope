c3493a47f3bd2cb1fca2464483c7566098f05bae
fluetanol <mike415415@naver.com>
2024-04-24 23:11:52 +0900

    Feat) 애니메이션 (JUMP+LANDING) 추가, IDLE모션 살짝 변경
    점프, 착지 애니메이션 추가
    
    (현재 state 구조 리팩토링 중)
    bug : 리팩토링 진행중에 koyote time이 작동 안되는 중

--- Assets/Script/PlayerAnimationManager.cs
+++ Assets/Script/PlayerAnimationManager.cs
@@ -7,8 +7,8 @@ public class PlayerAnimationManager : MonoBehaviour
 {
     private Animator _playerAnimator;
     private SpriteRenderer _playerSprite;
-    // Update is called once per frame
 
+    // Update is called once per frame
     void Start(){
         _playerAnimator = GetComponent<Animator>();
         _playerSprite = GetComponent<SpriteRenderer>();
@@ -19,7 +19,12 @@ public class PlayerAnimationManager : MonoBehaviour
     {
         float playSpeed = Math.Abs(PlayerStatus.CurrentMoveDirection.x * PlayerStatus.Speed * 1/8);
         _playerAnimator.SetFloat("speed", playSpeed);
-        if(PlayerStatus.CurrentMoveDirection.x < 0) _playerSprite.flipX = true;
+
+        if(PlayerStateManager.Instance.PlayerInputState == EPlayerInputState.Jump)  _playerAnimator.SetBool("isJump", true);
+        else _playerAnimator.SetBool("isJump", false);
+
+
+        if (PlayerStatus.CurrentMoveDirection.x < 0) _playerSprite.flipX = true;
         else if (PlayerStatus.CurrentMoveDirection.x > 0) _playerSprite.flipX = false;
     }
 }
